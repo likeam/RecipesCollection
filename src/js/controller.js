dd3965d3f0c9089574eaf2036a11ad8161873694
likeam <92205472+likeam@users.noreply.github.com>
2022-02-09 23:06:36 +0500

    ok

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,7 @@
-import * as model from './model';
-import recipeViews  from './views/recipeViews';
+import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 
 // import icons from '../img/icons.svg'; // Parcel 1
@@ -25,32 +27,49 @@ const timeout = function(s) {
 //////////////////////////
 
 
+import { async } from 'regenerator-runtime';
 
-const controlRecipe = async function() {
-        try {
-            const id = window.location.hash.slice(1);
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
 
-            if (!id) retun;
+    if (!id) return;
+    recipeView.renderSpinner();
 
-            recipeViews.rendeSpineer();
-            // 1) Loading Recipe
-            await model.loadRecipe(id);
-            
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
 
+    // 1) Updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
 
-            // 2) RENDERING RECIPE
+    // 2) Loading recipe
+    await model.loadRecipe(id);
 
-            recipeViews.render(model.state.recipe);
-            
-            
-            
-
-
-        } catch (err){
-        alert(err);
-    }
+    // 3) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+    console.error(err);
+  }
 };
-['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipe));
 
-// window.addEventListener('hashchange', controlRecipe);
-// window.addEventListener('load', controlRecipe);
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
